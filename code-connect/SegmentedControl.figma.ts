d725932929793099ea86c3dbb11ec6e2193c75e2
Duy Nguyen <duy28092005yh@gmail.com>
2026-08-04 00:01:27 +0700

    docs: connect SegmentedControl to Figma

---
+++ code-connect/SegmentedControl.figma.ts
@@ -0,0 +1,27 @@
+// url=https://www.figma.com/design/QJGevc16EkYfnOttxan2iK/Arcaea-Viewer-%E2%80%94-MVP-UI-Foundation?node-id=81-45
+import figma from "figma";
+
+const instance = figma.selectedInstance;
+const value = instance.getEnum("Value", {
+  Comfortable: '"comfortable"',
+  Compact: '"compact"',
+});
+const disabled = instance.getEnum("State", {
+  Disabled: "true",
+  Default: "false",
+  "Focus-visible": "false",
+});
+
+export default {
+  example: figma.code`
+    <SegmentedControl
+      label="Result density"
+      value={${value}}
+      options={densityOptions}
+      disabled={${disabled}}
+      onValueChange={setDensity}
+    />
+  `,
+  imports: ['import { SegmentedControl } from "./src/ui/SegmentedControl"'],
+  id: "arcaea-viewer-segmented-control",
+};
