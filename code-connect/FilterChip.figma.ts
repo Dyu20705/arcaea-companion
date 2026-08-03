c0f62a9fa2b722da9e77c82bd8efa4b53b09fd7c
Duy Nguyen <duy28092005yh@gmail.com>
2026-08-04 00:00:50 +0700

    docs: connect FilterChip to Figma

---
+++ code-connect/FilterChip.figma.ts
@@ -0,0 +1,28 @@
+// url=https://www.figma.com/design/QJGevc16EkYfnOttxan2iK/Arcaea-Viewer-%E2%80%94-MVP-UI-Foundation?node-id=21-193
+import figma from "figma";
+
+const instance = figma.selectedInstance;
+const selected = instance.getEnum("Selected", {
+  Yes: "true",
+  No: "false",
+});
+const disabled = instance.getEnum("State", {
+  Disabled: "true",
+  Default: "false",
+  Hover: "false",
+  "Focus-visible": "false",
+});
+
+export default {
+  example: figma.code`
+    <FilterChip
+      label="Future"
+      selected={${selected}}
+      disabled={${disabled}}
+      onSelectedChange={setFutureSelected}
+      onRemove={removeFutureFilter}
+    />
+  `,
+  imports: ['import { FilterChip } from "./src/ui/FilterChip"'],
+  id: "arcaea-viewer-filter-chip",
+};
